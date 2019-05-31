8c7ef09b4f1d11e648b3806b25ad2571feb79bfd
carolynrehm <carolynhrehm@gmail.com>
2019-05-31 08:43:58 -0500

    update service

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -225,30 +225,9 @@ public class EvaluationService {
 
 	}
 
-	/**
-	 * 8. Implement a program that translates from English to Pig Latin.
-	 * 
-	 * Pig Latin is a made-up children's language that's intended to be confusing.
-	 * It obeys a few simple rules (below), but when it's spoken quickly it's really
-	 * difficult for non-children (and non-native speakers) to understand.
-	 * 
-	 * Rule 1: If a word begins with a vowel sound, add an "ay" sound to the end of
-	 * the word. Rule 2: If a word begins with a consonant sound, move it to the end
-	 * of the word, and then add an "ay" sound to the end of the word. There are a
-	 * few more rules for edge cases, and there are regional variants too.
-	 * 
-	 * See http://en.wikipedia.org/wiki/Pig_latin for more details.
-	 * 
-	 * @param string
-	 * @return
-	 */
-	public String toPigLatin(String string) {
-		// TODO Write an implementation for this method declaration
-		return null;
-	}
 
 	/**
-	 * 9. An Armstrong number is a number that is the sum of its own digits each
+	 * 8. An Armstrong number is a number that is the sum of its own digits each
 	 * raised to the power of the number of digits.
 	 * 
 	 * For example:
@@ -268,22 +247,26 @@ public class EvaluationService {
 	}
 
 	/**
-	 * 10. Compute the prime factors of a given natural number.
+	 * 9. Determine if a sentence is a pangram. A pangram (Greek: παν γράμμα, pan
+	 * gramma, "every letter") is a sentence using every letter of the alphabet at
+	 * least once. The best known English pangram is:
 	 * 
-	 * A prime number is only evenly divisible by itself and 1.
+	 * The quick brown fox jumps over the lazy dog.
 	 * 
-	 * Note that 1 is not a prime number.
+	 * The alphabet used consists of ASCII letters a to z, inclusive, and is case
+	 * insensitive. Input will not contain non-ASCII symbols.
 	 * 
-	 * @param l
+	 * @param string
 	 * @return
 	 */
-	public List<Long> calculatePrimeFactorsOf(long l) {
+	public boolean isPangram(String string) {
 		// TODO Write an implementation for this method declaration
-		return null;
+		return false;
 	}
 
+	
 	/**
-	 * 11. Create an implementation of the rotational cipher, also sometimes called
+	 * 10. Create an implementation of the rotational cipher, also sometimes called
 	 * the Caesar cipher.
 	 * 
 	 * The Caesar cipher is a simple shift cipher that relies on transposing all the
@@ -322,26 +305,10 @@ public class EvaluationService {
 		}
 
 	}
+	
 
 	/**
-	 * 12. Given a number n, determine what the nth prime is.
-	 * 
-	 * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
-	 * that the 6th prime is 13.
-	 * 
-	 * If your language provides methods in the standard library to deal with prime
-	 * numbers, pretend they don't exist and implement them yourself.
-	 * 
-	 * @param i
-	 * @return
-	 */
-	public int calculateNthPrime(int i) {
-		// TODO Write an implementation for this method declaration
-		return 0;
-	}
-
-	/**
-	 * 13 & 14. Create an implementation of the atbash cipher, an ancient encryption
+	 * 11 & 12. Create an implementation of the atbash cipher, an ancient encryption
 	 * system created in the Middle East.
 	 * 
 	 * The Atbash cipher is a simple substitution cipher that relies on transposing
@@ -367,7 +334,7 @@ public class EvaluationService {
 	static class AtbashCipher {
 
 		/**
-		 * Question 13
+		 * Question 11
 		 * 
 		 * @param string
 		 * @return
@@ -378,7 +345,7 @@ public class EvaluationService {
 		}
 
 		/**
-		 * Question 14
+		 * Question 12
 		 * 
 		 * @param string
 		 * @return
@@ -390,7 +357,7 @@ public class EvaluationService {
 	}
 
 	/**
-	 * 15. The ISBN-10 verification process is used to validate book identification
+	 * 13. (Optional) The ISBN-10 verification process is used to validate book identification
 	 * numbers. These normally contain dashes and look like: 3-598-21508-8
 	 * 
 	 * ISBN The ISBN-10 format is 9 digits (0 to 9) plus one check character (either
@@ -416,26 +383,9 @@ public class EvaluationService {
 		return false;
 	}
 
-	/**
-	 * 16. Determine if a sentence is a pangram. A pangram (Greek: παν γράμμα, pan
-	 * gramma, "every letter") is a sentence using every letter of the alphabet at
-	 * least once. The best known English pangram is:
-	 * 
-	 * The quick brown fox jumps over the lazy dog.
-	 * 
-	 * The alphabet used consists of ASCII letters a to z, inclusive, and is case
-	 * insensitive. Input will not contain non-ASCII symbols.
-	 * 
-	 * @param string
-	 * @return
-	 */
-	public boolean isPangram(String string) {
-		// TODO Write an implementation for this method declaration
-		return false;
-	}
 
 	/**
-	 * 17. Calculate the moment when someone has lived for 10^9 seconds.
+	 * 14. (Optional) Calculate the moment when someone has lived for 10^9 seconds.
 	 * 
 	 * A gigasecond is 109 (1,000,000,000) seconds.
 	 * 
@@ -447,67 +397,9 @@ public class EvaluationService {
 		return null;
 	}
 
+	
 	/**
-	 * 18. Given a number, find the sum of all the unique multiples of particular
-	 * numbers up to but not including that number.
-	 * 
-	 * If we list all the natural numbers below 20 that are multiples of 3 or 5, we
-	 * get 3, 5, 6, 9, 10, 12, 15, and 18.
-	 * 
-	 * The sum of these multiples is 78.
-	 * 
-	 * @param i
-	 * @param set
-	 * @return
-	 */
-	public int getSumOfMultiples(int i, int[] set) {
-		// TODO Write an implementation for this method declaration
-		return 0;
-	}
-
-	/**
-	 * 19. Given a number determine whether or not it is valid per the Luhn formula.
-	 * 
-	 * The Luhn algorithm is a simple checksum formula used to validate a variety of
-	 * identification numbers, such as credit card numbers and Canadian Social
-	 * Insurance Numbers.
-	 * 
-	 * The task is to check if a given string is valid.
-	 * 
-	 * Validating a Number Strings of length 1 or less are not valid. Spaces are
-	 * allowed in the input, but they should be stripped before checking. All other
-	 * non-digit characters are disallowed.
-	 * 
-	 * Example 1: valid credit card number 1 4539 1488 0343 6467 The first step of
-	 * the Luhn algorithm is to double every second digit, starting from the right.
-	 * We will be doubling
-	 * 
-	 * 4_3_ 1_8_ 0_4_ 6_6_ If doubling the number results in a number greater than 9
-	 * then subtract 9 from the product. The results of our doubling:
-	 * 
-	 * 8569 2478 0383 3437 Then sum all of the digits:
-	 * 
-	 * 8+5+6+9+2+4+7+8+0+3+8+3+3+4+3+7 = 80 If the sum is evenly divisible by 10,
-	 * then the number is valid. This number is valid!
-	 * 
-	 * Example 2: invalid credit card number 1 8273 1232 7352 0569 Double the second
-	 * digits, starting from the right
-	 * 
-	 * 7253 2262 5312 0539 Sum the digits
-	 * 
-	 * 7+2+5+3+2+2+6+2+5+3+1+2+0+5+3+9 = 57 57 is not evenly divisible by 10, so
-	 * this number is not valid.
-	 * 
-	 * @param string
-	 * @return
-	 */
-	public boolean isLuhnValid(String string) {
-		// TODO Write an implementation for this method declaration
-		return false;
-	}
-
-	/**
-	 * 20. Parse and evaluate simple math word problems returning the answer as an
+	 * 15. (Optional) Parse and evaluate simple math word problems returning the answer as an
 	 * integer.
 	 * 
 	 * Add two numbers together.
--- src/test/java/com/revature/eval/java/core/EvaluationServiceTest.java
+++ src/test/java/com/revature/eval/java/core/EvaluationServiceTest.java
@@ -1,9 +1,7 @@
 package com.revature.eval.java.core;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
@@ -15,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -303,34 +302,6 @@ public class EvaluationServiceTest {
 	 * Question 8
 	 ******************************************************************/
 	@Test
-	public void testWordBeginningWithA() {
-		assertEquals("appleay", evaluationService.toPigLatin("apple"));
-	}
-
-	@Test
-	public void testThTreatedLikeAConsonantAtTheBeginningOfAWord() {
-		assertEquals("erapythay", evaluationService.toPigLatin("therapy"));
-	}
-
-	@Test
-	public void testSchTreatedLikeAConsonantAtTheBeginningOfAWord() {
-		assertEquals("oolschay", evaluationService.toPigLatin("school"));
-	}
-
-	@Test
-	public void testYTreatedLikeAConsonantAtTheBeginningOfAWord() {
-		assertEquals("ellowyay", evaluationService.toPigLatin("yellow"));
-	}
-
-	@Test
-	public void testAWholePhrase() {
-		assertEquals("ickquay astfay unray", evaluationService.toPigLatin("quick fast run"));
-	}
-
-	/*******************************************************************
-	 * Question 9
-	 ******************************************************************/
-	@Test
 	public void singleDigitsAreArmstrongNumbers() {
 		int input = 5;
 
@@ -364,38 +335,38 @@ public class EvaluationServiceTest {
 
 		assertTrue(evaluationService.isArmstrongNumber(input));
 	}
+	
+	/*******************************************************************
+	 * Question 9
+	 ******************************************************************/
+	@Test
+	public void emptySentenceIsNotPangram() {
+		assertFalse(evaluationService.isPangram(""));
+	}
 
+	@Test
+	public void recognizesPerfectLowerCasePangram() {
+		assertTrue(evaluationService.isPangram("abcdefghijklmnopqrstuvwxyz"));
+	}
+
+	@Test
+	public void pangramWithOnlyLowerCaseLettersIsRecognizedAsPangram() {
+		assertTrue(evaluationService.isPangram("the quick brown fox jumps over the lazy dog"));
+	}
+
+	@Test
+	public void phraseMissingCharacterXIsNotPangram() {
+		assertFalse(evaluationService.isPangram("a quick movement of the enemy will jeopardize five gunboats"));
+	}
+
+	@Test
+	public void phraseMissingAnotherCharacterIsNotPangram() {
+		assertFalse(evaluationService.isPangram("five boxing wizards jump quickly at it"));
+	}
+
+	
 	/*******************************************************************
 	 * Question 10
-	 ******************************************************************/
-
-	@Test
-	public void testPrimeNumber() {
-		assertEquals(Collections.singletonList(2L), evaluationService.calculatePrimeFactorsOf(2L));
-	}
-
-	@Test
-	public void testSquareOfAPrime() {
-		assertEquals(Arrays.asList(3L, 3L), evaluationService.calculatePrimeFactorsOf(9L));
-	}
-
-	@Test
-	public void testCubeOfAPrime() {
-		assertEquals(Arrays.asList(2L, 2L, 2L), evaluationService.calculatePrimeFactorsOf(8L));
-	}
-
-	@Test
-	public void testProductOfPrimesAndNonPrimes() {
-		assertEquals(Arrays.asList(2L, 2L, 3L), evaluationService.calculatePrimeFactorsOf(12L));
-	}
-
-	@Test
-	public void testProductOfPrimes() {
-		assertEquals(Arrays.asList(5L, 17L, 23L, 461L), evaluationService.calculatePrimeFactorsOf(901255L));
-	}
-
-	/*******************************************************************
-	 * Question 11
 	 ******************************************************************/
 
 	@Test
@@ -428,38 +399,10 @@ public class EvaluationServiceTest {
 		assertEquals("The quick brown fox jumps over the lazy dog.",
 				rotationalCipher.rotate("Gur dhvpx oebja sbk whzcf bire gur ynml qbt."));
 	}
+	
 
 	/*******************************************************************
-	 * Question 12
-	 ******************************************************************/
-	@Test
-	public void testFirstPrime() {
-		assertThat(evaluationService.calculateNthPrime(1), is(2));
-	}
-
-	@Test
-	public void testSecondPrime() {
-		assertThat(evaluationService.calculateNthPrime(2), is(3));
-	}
-
-	@Test
-	public void testSixthPrime() {
-		assertThat(evaluationService.calculateNthPrime(6), is(13));
-	}
-
-	@Test
-	public void testBigPrime() {
-		assertThat(evaluationService.calculateNthPrime(10001), is(104743));
-	}
-
-	@Test
-	public void testUndefinedPrime() {
-		expectedException.expect(IllegalArgumentException.class);
-		evaluationService.calculateNthPrime(0);
-	}
-
-	/*******************************************************************
-	 * Question 13
+	 * Question 11
 	 ******************************************************************/
 
 	@Test
@@ -494,7 +437,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 14
+	 * Question 12
 	 ******************************************************************/
 	@Test
 	public void testDecodeExercism() {
@@ -519,194 +462,106 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 15
+	 * Question 13
 	 ******************************************************************/
 	@Test
+	@Ignore
 	public void validIsbnNumber() {
 		assertTrue(evaluationService.isValidIsbn("3-598-21508-8"));
 	}
 
 	@Test
+	@Ignore
 	public void invalidIsbnCheckDigit() {
 		assertFalse(evaluationService.isValidIsbn("3-598-21508-9"));
 	}
 
 	@Test
+	@Ignore
 	public void validIsbnNumberWithCheckDigitOfTen() {
 		assertTrue(evaluationService.isValidIsbn("3-598-21507-X"));
 	}
 
 	@Test
+	@Ignore
 	public void checkDigitIsACharacterOtherThanX() {
 		assertFalse(evaluationService.isValidIsbn("3-598-21507-A"));
 	}
 
 	@Test
+	@Ignore
 	public void invalidCharacterInIsbn() {
 		assertFalse(evaluationService.isValidIsbn("3-598-2K507-0"));
 	}
 
-	/*******************************************************************
-	 * Question 16
-	 ******************************************************************/
-	@Test
-	public void emptySentenceIsNotPangram() {
-		assertFalse(evaluationService.isPangram(""));
-	}
-
-	@Test
-	public void recognizesPerfectLowerCasePangram() {
-		assertTrue(evaluationService.isPangram("abcdefghijklmnopqrstuvwxyz"));
-	}
-
-	@Test
-	public void pangramWithOnlyLowerCaseLettersIsRecognizedAsPangram() {
-		assertTrue(evaluationService.isPangram("the quick brown fox jumps over the lazy dog"));
-	}
-
-	@Test
-	public void phraseMissingCharacterXIsNotPangram() {
-		assertFalse(evaluationService.isPangram("a quick movement of the enemy will jeopardize five gunboats"));
-	}
-
-	@Test
-	public void phraseMissingAnotherCharacterIsNotPangram() {
-		assertFalse(evaluationService.isPangram("five boxing wizards jump quickly at it"));
-	}
 
 	/*******************************************************************
-	 * Question 17
+	 * Question 14
 	 ******************************************************************/
 	@Test
+	@Ignore
 	public void modernTime() {
 		assertEquals(LocalDateTime.of(2043, Month.JANUARY, 1, 1, 46, 40),
 				evaluationService.getGigasecondDate(LocalDate.of(2011, Month.APRIL, 25)));
 	}
 
 	@Test
+	@Ignore
 	public void afterEpochTime() {
 		assertEquals(LocalDateTime.of(2009, Month.FEBRUARY, 19, 1, 46, 40),
 				evaluationService.getGigasecondDate(LocalDate.of(1977, Month.JUNE, 13)));
 	}
 
 	@Test
+	@Ignore
 	public void beforeEpochTime() {
 		assertEquals(LocalDateTime.of(1991, Month.MARCH, 27, 1, 46, 40),
 				evaluationService.getGigasecondDate(LocalDate.of(1959, Month.JULY, 19)));
 	}
 
 	@Test
+	@Ignore
 	public void withFullTimeSpecified() {
 		assertEquals(LocalDateTime.of(2046, Month.OCTOBER, 2, 23, 46, 40),
 				evaluationService.getGigasecondDate(LocalDateTime.of(2015, Month.JANUARY, 24, 22, 0, 0)));
 	}
 
 	@Test
+	@Ignore
 	public void withFullTimeSpecifiedAndDayRollover() {
 		assertEquals(LocalDateTime.of(2046, Month.OCTOBER, 3, 1, 46, 39),
 				evaluationService.getGigasecondDate(LocalDateTime.of(2015, Month.JANUARY, 24, 23, 59, 59)));
 	}
 
 	/*******************************************************************
-	 * Question 18
+	 * Question 15
 	 ******************************************************************/
 	@Test
-	public void testSumOfMultiplesOf4and6UpToFifteen() {
-
-		int[] set = { 4, 6 };
-		int output = evaluationService.getSumOfMultiples(15, set);
-		assertEquals(30, output);
-
-	}
-
-	@Test
-	public void testSumOfMultiplesOf5and6and8UpToOneHundredFifty() {
-
-		int[] set = { 5, 6, 8 };
-		int output = evaluationService.getSumOfMultiples(150, set);
-		assertEquals(4419, output);
-
-	}
-
-	@Test
-	public void testSumOfMultiplesOf5and25UpToFiftyOne() {
-
-		int[] set = { 5, 25 };
-		int output = evaluationService.getSumOfMultiples(51, set);
-		assertEquals(275, output);
-
-	}
-
-	@Test
-	public void testSumOfMultiplesOf43and47UpToTenThousand() {
-
-		int[] set = { 43, 47 };
-		int output = evaluationService.getSumOfMultiples(10000, set);
-		assertEquals(2203160, output);
-
-	}
-
-	@Test
-	public void testSumOfMultiplesOfOneUpToOneHundred() {
-
-		int[] set = { 1 };
-		int output = evaluationService.getSumOfMultiples(100, set);
-		assertEquals(4950, output);
-
-	}
-
-	/*******************************************************************
-	 * Question 19
-	 ******************************************************************/
-	@Test
-	public void testThatAValidCanadianSocialInsuranceNumberIsIdentifiedAsValidV1() {
-		assertTrue(evaluationService.isLuhnValid("046 454 286"));
-	}
-
-	@Test
-	public void testThatAnInvalidCanadianSocialInsuranceNumberIsIdentifiedAsInvalid() {
-		assertFalse(evaluationService.isLuhnValid("046 454 287"));
-	}
-
-	@Test
-	public void testThatAnInvalidCreditCardIsIdentifiedAsInvalid() {
-		assertFalse(evaluationService.isLuhnValid("8273 1232 7352 0569"));
-	}
-
-	@Test
-	public void testThatAddingANonDigitCharacterToAValidStringInvalidatesTheString() {
-		assertFalse(evaluationService.isLuhnValid("046a 454 286"));
-	}
-
-	@Test
-	public void testThatStringContainingPunctuationIsInvalid() {
-		assertFalse(evaluationService.isLuhnValid("055-444-285"));
-	}
-
-	/*******************************************************************
-	 * Question 20
-	 ******************************************************************/
-	@Test
+	@Ignore
 	public void testSingleAddition1() {
 		assertEquals(2, evaluationService.solveWordProblem("What is 1 plus 1?"));
 	}
 
 	@Test
+	@Ignore
 	public void testSingleAdditionWithNegativeNumbers() {
 		assertEquals(-11, evaluationService.solveWordProblem("What is -1 plus -10?"));
 	}
 
 	@Test
+	@Ignore
 	public void testSingleSubtraction() {
 		assertEquals(16, evaluationService.solveWordProblem("What is 4 minus -12?"));
 	}
 
 	@Test
+	@Ignore
 	public void testSingleMultiplication() {
 		assertEquals(-75, evaluationService.solveWordProblem("What is -3 multiplied by 25?"));
 	}
 
 	@Test
+	@Ignore
 	public void testSingleDivision() {
 		assertEquals(-11, evaluationService.solveWordProblem("What is 33 divided by -3?"));
 	}
